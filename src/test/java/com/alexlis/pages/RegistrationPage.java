bbbc5aabc60c24659d8d3571716e827069bf266e
alekseilisenkov <alexlisenkov92@mail.ru>
2022-07-15 20:31:21 +0300

    another build

--- src/test/java/com/alexlis/pages/RegistrationPage.java
+++ src/test/java/com/alexlis/pages/RegistrationPage.java
@@ -1,9 +1,7 @@
 package com.alexlis.pages;
 
 import com.alexlis.pages.components.CalendarComponent;
-import com.alexlis.pages.components.CityComponent;
-import com.alexlis.pages.components.StateComponent;
-
+import com.alexlis.pages.components.StateAndCityComponent;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
@@ -13,30 +11,27 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationPage {
 
-    private final String FORM_TITLE = "Student Registration Form";
-    public CalendarComponent selectCalendar = new CalendarComponent();
-    public StateComponent stateComponent = new StateComponent();
-    public CityComponent cityComponent = new CityComponent();
+    private final String registrationFormUrl = "https://demoqa.com/automation-practice-form";
 
-    private SelenideElement
-            formTitle = $(".practice-form-wrapper"),
-            firstNameInput = $("#firstName"),
-            lastNameInput = $("#lastName"),
-            emailInput = $("#userEmail"),
-            currentAddress = $("#currentAddress"),
-            genderButton = $("#genterWrapper"),
-            hobbiesButton = $("#hobbiesWrapper"),
-            inputPhone = $("#userNumber"),
-            inputSience = $("#subjectsInput"),
-            uploadPicturepath = $("#uploadPicture"),
-            clickPath = $("#submit"),
-            assertPage = $(".modal-title"),
-            assertTable = $(".table-responsive");
+    private SelenideElement firstNameInput = $("#firstName"),
+                            lastNameInput = $("#lastName"),
+                            emailInput = $("#userEmail"),
+                            genderCheckbox = $("#genterWrapper"),
+                            phoneNumberInput = $("#userNumber"),
+                            subjectWrapper = $("#subjectsInput"),
+                            hobbiesCheckbox = $("#hobbiesWrapper"),
+                            uploadPictureWrapper = $("#uploadPicture"),
+                            addressInput = $("#currentAddress"),
+                            submitBtn = $("#submit"),
+                            resultsTable = $(".table-responsive");
 
+    public CalendarComponent calendar = new CalendarComponent();
+    public StateAndCityComponent stateAndCity = new StateAndCityComponent();
 
-    public void openPage() {
-        open("/automation-practice-form");
-        formTitle.shouldHave(text(FORM_TITLE));
+    // ACTIONS
+    public RegistrationPage openPage() {
+        open(registrationFormUrl);
+        return this;
     }
 
     public RegistrationPage typeFirstName(String value) {
@@ -54,47 +49,58 @@ public class RegistrationPage {
         return this;
     }
 
-    public RegistrationPage typeAddress(String value) {
-        currentAddress.setValue(value);
-        return this;
-    }
-
     public RegistrationPage chooseGender(String value) {
-        genderButton.$(byText(value)).click();
+        genderCheckbox.$(byText(value)).click();
         return this;
     }
 
-    public RegistrationPage chooseHobbies(String value) {
-        hobbiesButton.$(byText(value)).click();
+    public RegistrationPage typePhoneNumber(String value) {
+        phoneNumberInput.setValue(value);
         return this;
     }
 
-    public RegistrationPage choosePhone(String value) {
-        inputPhone.setValue(value).click();
+    public RegistrationPage setBirthdayDate(String year, String month, String day) {
+        calendar.setDate(year, month, day);
+        return this;
+    }
+    public RegistrationPage chooseSubject(String value) {
+        subjectWrapper.setValue(value).pressEnter();
         return this;
     }
 
-    public RegistrationPage chooseSience(String value) {
-        inputSience.setValue(value).pressEnter();
+    public RegistrationPage setHobbies(String value) {
+        hobbiesCheckbox.$(byText(value)).click();
         return this;
     }
 
     public RegistrationPage uploadPicture(String value) {
-        uploadPicturepath.uploadFromClasspath(value);
+        uploadPictureWrapper.uploadFromClasspath(value);
         return this;
     }
 
-    public RegistrationPage clickButton() {
-        clickPath.click();
+    public RegistrationPage typeAddress(String value) {
+        addressInput.setValue(value);
         return this;
     }
 
-    public void finalAssert() {
-        assertPage.shouldHave(text("Thanks for submitting the form"));
+    public RegistrationPage setState(String value) {
+        stateAndCity.setState(value);
+        return this;
     }
 
-    public void finalAssertOfTable() {
-        assertTable.shouldHave(text("Alexei Lisenkov"), text("alexlis@mail.ru"), text("Male"), text("8999666555"),
-                text("Saint-Petersburg"), text("NCR Delhi"));
+    public RegistrationPage setCity(String value) {
+        stateAndCity.setCity(value);
+        return this;
+    }
+
+    public void submitForm() {
+        submitBtn.click();
+    }
+
+    public RegistrationPage checkResultsValue(String key, String value) {
+        resultsTable.$(byText(key))
+                .parent().shouldHave(text(value));
+
+        return this;
     }
 }
--- src/test/java/com/alexlis/pages/components/CalendarComponent.java
+++ src/test/java/com/alexlis/pages/components/CalendarComponent.java
@@ -1,13 +1,19 @@
 package com.alexlis.pages.components;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class CalendarComponent {
+    private SelenideElement calendarFormWrapper = $("#dateOfBirthInput");
 
-    public void setDate(String day, String month, String year) {
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(month);
+    public void setDate(String year, String month, String day) {
+        calendarFormWrapper.click();
         $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $$(".react-datepicker__day").find(exactText(day)).click();
+
     }
 }
