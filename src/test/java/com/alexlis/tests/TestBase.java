cc6b476e53d7628bc27e64ad6479fad98542b1ca
alekseilisenkov <alexlisenkov92@mail.ru>
2022-07-16 23:09:35 +0300

    another build

--- src/test/java/com/alexlis/tests/TestBase.java
+++ src/test/java/com/alexlis/tests/TestBase.java
@@ -3,6 +3,9 @@ package com.alexlis.tests;
 import com.alexlis.helpers.Attach;
 import com.codeborne.selenide.Configuration;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -17,12 +20,11 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-
-
     }
 
     @AfterEach
