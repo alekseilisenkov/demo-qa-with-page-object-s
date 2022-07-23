02ae78e087bfbb357dfe3dbd6eab7e9c7e7e69f5
alekseilisenkov <alexlisenkov92@mail.ru>
2022-07-23 22:07:48 +0300

    add owner test

--- src/test/java/com/alexlis/properties/OwnerTests.java
+++ src/test/java/com/alexlis/properties/OwnerTests.java
@@ -3,6 +3,7 @@ package com.alexlis.properties;
 import com.alexlis.config.CredentialsConfig;
 import org.aeonbits.owner.Config;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
@@ -10,6 +11,7 @@ import static java.lang.String.format;
 public class OwnerTests {
 public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
 
+    @Tag("properties")
     @Test
     void readCredentialsTest() {
         String login = credentials.login();
